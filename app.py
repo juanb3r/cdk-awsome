19f95af4a8b2a522476198238842aee41a861387
juan <juanmiber@gmail.com>
2023-02-11 09:35:36 -0500

    Se hace inicialización de proyecto, y se generan configuraciones para el pipeline

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from os import getenv
+
+import aws_cdk as cdk
+
+from my_pipeline.my_pipeline_stack import MyPipelineStack
+
+
+app = cdk.App()
+MyPipelineStack(
+    app,
+    "MyPipelineStack",
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    env=cdk.Environment(
+        account=getenv('CDK_DEFAULT_ACCOUNT'),
+        region=getenv('CDK_DEFAULT_REGION')
+    ),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
+
+app.synth()
