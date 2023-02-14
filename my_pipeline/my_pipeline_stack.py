f9538badb2c80cb807f1550b2f2cf78249062552
juan <juanmiber@gmail.com>
2023-02-14 07:25:42 -0500

    Se crea stage test, y se agrega stack de lambda

--- my_pipeline/my_pipeline_stack.py
+++ my_pipeline/my_pipeline_stack.py
@@ -1,8 +1,12 @@
+from os import getenv
+
+import aws_cdk as cdk
 from aws_cdk import (
     Stack,
 )
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from constructs import Construct
+from my_pipeline.my_pipeline_app_stage import MyPipelineAppStage
 
 
 class MyPipelineStack(Stack):
@@ -10,7 +14,7 @@ class MyPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        pipeline=CodePipeline(
+        pipeline = CodePipeline(
             self,
             "Pipeline",
             pipeline_name="MyPipeline",
@@ -25,5 +29,16 @@ class MyPipelineStack(Stack):
                     "python -m pip install -r requirements.txt",
                     "cdk synth"
                 ]
+            )
+        )
+
+        pipeline.add_stage(
+            MyPipelineAppStage(
+                self,
+                "test",
+                env=cdk.Environment(
+                    account=getenv('CDK_DEFAULT_ACCOUNT'),
+                    region=getenv('CDK_DEFAULT_REGION')
+                )
             )
         )
